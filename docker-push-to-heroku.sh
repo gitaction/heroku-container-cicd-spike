65a2356d7f51fd656970f34a4061031b2aec9351
sunwei <me@sunwei.xyz>
2022-01-12 09:38:04 +0800

    Modified: read token from stdin; Wayde

--- docker-push-to-heroku.sh
+++ docker-push-to-heroku.sh
@@ -3,8 +3,7 @@ set -e
 
 echo "> Pushing image to heroku image registry..."
 
-echo "${HEROKU_TOKEN}" | \
-docker login --username=_ --password-stdin registry.heroku.com
+echo "${HEROKU_TOKEN}" | docker login --username=_ --password-stdin registry.heroku.com
 
 docker tag heroku-container-cicd-spike:latest registry.heroku.com/heroku-container-cicd-spike/web:latest
 docker push registry.heroku.com/heroku-container-cicd-spike/web:latest
